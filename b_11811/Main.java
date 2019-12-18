b188a1bb183ef446ca92c19460c02402aca8b129
Kim, Junha <kimjh4930@gmail.com>
2019-12-18 23:58:12 +0900

    11811. 데스스타

--- b_11811/Main.java
+++ b_11811/Main.java
@@ -10,29 +10,19 @@ public class Main {
 		
 		int N = Integer.parseInt(br.readLine());
 		int value = 0;
-		int[] inputnum = new int[N];
 		String[] input;
-		
 		StringBuffer result = new StringBuffer();
 		
 		for(int i=0; i<N; i++) {
 			value = 0;
-			
 			input = br.readLine().split(" ");
 			for(int j=0; j<N; j++) {
-				inputnum[j] = Integer.parseInt(input[j]);
+				value |= Integer.parseInt(input[j]);
 			}
-			
-			for(int j=0; j<N; j++) {
-				value |= inputnum[j];  
-			}
-			
 			result.append(value + " ");
-			
 		}
 		
 		System.out.println(result.toString());
-		
 		br.close();
 	}
 }
